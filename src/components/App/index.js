3adc6298f85e2ce06b6763d594daec8e0c6e6b7d
raitalharehman <talharaay@gmail.com>
2019-05-09 12:08:37 +0500

    init

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './scss/index.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
